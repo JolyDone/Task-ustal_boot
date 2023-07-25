f36e79917fe71f9a593b9affa28880a071e33aa5
JolyDone <twilightclimb@gmail.com>
2023-07-25 17:55:41 +0300

    красота

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -5,9 +5,9 @@ import web.model.User;
 import java.util.List;
 
 public interface UserDao {
-    public List<User> findAll();
-    public User find(long id);
-    public void save(User user);
-    public void delete(long uid);
-    public void update(long id, User user);
+    List<User> findAll();
+    User find(long id);
+    void save(User user);
+    void delete(long uid);
+    void update(long id, User user);
 }
